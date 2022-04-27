8ba91c80e8e3afdf65fb554ba8d0339011871a3c
Guilherme Gomes <ggs.arquiteto@gmail.com>
2022-04-27 17:31:46 -0300

    feat: implementa a migration de sales

--- back-end/src/database/migrations/20220427201720-sales.js
+++ back-end/src/database/migrations/20220427201720-sales.js
@@ -2,20 +2,57 @@
 
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+    await queryInterface.createTable('sales', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      user_id: {
+        allowNull: false,
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+        references: {
+          model: 'users',
+          key: 'id',
+        },
+        type: Sequelize.INTEGER,
+      },
+      seller_id: {
+        allowNull: false,
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+        references: {
+          model: 'users',
+          key: 'id',
+        },
+        type: Sequelize.INTEGER,
+      },
+      total_price: {
+        allowNull: false,
+        type: Sequelize.DECIMAL(9,2),
+      },
+      delivery_address: {
+        allowNull: false,
+        type: Sequelize.STRING({ length: 100 }),
+      },
+      delivery_number: {
+        allowNull: false,
+        type: Sequelize.STRING({ length: 50 }),
+      },
+      sale_date: { 
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      status: {
+        allowNull: false,
+        type: Sequelize.STRING({ length: 50 }),
+      }
+    });
   },
 
   async down (queryInterface, Sequelize) {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+    await queryInterface.dropTable('sales');
   }
 };
