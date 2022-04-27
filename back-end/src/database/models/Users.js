8af47c2f903ea2e6d5ba52e20e3563552a84c6fd
Guilherme Gomes <ggs.arquiteto@gmail.com>
2022-04-27 16:29:24 -0300

    feat: implementa o model de users

---
+++ back-end/src/database/models/Users.js
@@ -0,0 +1,36 @@
+const { DataTypes } = require("sequelize/types");
+
+module.exports = (sequelize, DataTypes) => {
+  const Users = sequelize.define(
+    "Users",
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      role: {
+        type: DataTypes.STRING,
+        allowNull: false
+      }
+    },
+    {
+      tableName: "users",
+      timestamps: false
+    }    
+  );
+  return Users;
+}
