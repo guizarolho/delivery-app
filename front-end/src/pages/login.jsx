b0936928b7e49b6b06298d6ecd6e54fa234716b5
Thiago Frozzi <thiagofrozzi@hotmail.com>
2022-04-27 16:25:00 -0300

    build: instal react router dom 6

---
+++ front-end/src/pages/login.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <span className="logo">CETABOM ENTREGAS!</span>
+
+      <label htmlFor=''> Login
+        <input type="text" placeholder='email@trybeer.com.br'></input>
+      </label>
+
+      <label htmlFor=''> Senha
+        <input type="password" placeholder='*******'></input>
+      </label>
+      
+    </div>
+  );
+}
+
+export default App;
