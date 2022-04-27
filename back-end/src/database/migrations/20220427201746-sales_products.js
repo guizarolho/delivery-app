00c96354a448217b641e7c58e3cb8592ca62701a
Guilherme Gomes <ggs.arquiteto@gmail.com>
2022-04-27 17:36:46 -0300

    feat: implementa a migration de sales_products

--- back-end/src/database/migrations/20220427201746-sales_products.js
+++ back-end/src/database/migrations/20220427201746-sales_products.js
@@ -2,20 +2,43 @@
 
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+    await queryInterface.createTable('sales_products', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      sale_id: {
+        allowNull: false,
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+        references: {
+          model: 'sales',
+          key: 'id',
+        },
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      product_id: {
+        allowNull: false,
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+        references: {
+          model: 'products',
+          key: 'id',
+        },
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      quantity: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+      }
+    });
   },
 
   async down (queryInterface, Sequelize) {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+    await queryInterface.dropTable('sales_products');
   }
 };
