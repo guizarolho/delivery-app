45a113a230eaefb2d2fdbdb5ea1c23153ac1fe3e
Guilherme Gomes <ggs.arquiteto@gmail.com>
2022-04-27 17:33:47 -0300

    feat: implementa a migration de products

--- back-end/src/database/migrations/20220427201732-products.js
+++ back-end/src/database/migrations/20220427201732-products.js
@@ -2,20 +2,29 @@
 
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+    await queryInterface.createTable('products', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      name: {
+        allowNull: false,
+        type: Sequelize.STRING({ length: 100 }),
+      },
+      price: {
+        allowNull: false,
+        type: Sequelize.DECIMAL(4,2),
+      },
+      url_image: {
+        allowNull: false,
+        type: Sequelize.STRING({ length: 200 }),
+      }
+    });
   },
 
   async down (queryInterface, Sequelize) {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+    await queryInterface.dropTable('products');
   }
 };
