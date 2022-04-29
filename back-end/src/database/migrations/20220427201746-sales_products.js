70284f7f5923ed9cb613d7ffd182eda51e1f8d8d
Thiago Frozzi <thiagofrozzi@hotmail.com>
2022-04-29 20:34:03 -0300

    fix: corrige erros de sequelize

--- back-end/src/database/migrations/20220427201746-sales_products.js
+++ back-end/src/database/migrations/20220427201746-sales_products.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = {
-  async up (queryInterface, Sequelize) {
+   up: async (queryInterface, Sequelize) => {
     await queryInterface.createTable('sales_products', {
       id: {
         allowNull: false,
@@ -38,7 +38,7 @@ module.exports = {
     });
   },
 
-  async down (queryInterface, Sequelize) {
+   down: async (queryInterface, Sequelize) => {
     await queryInterface.dropTable('sales_products');
   }
 };
--- back-end/src/services/userService.js
+++ back-end/src/services/userService.js
@@ -1,10 +1,10 @@
 const md5 = require('md5');
-const Users = require('../database/models/Users');
+const { Users } = require('../database/models');
 
 const login = async (email, password) => {
   const criptedPassword = md5(password);
-  console.log(criptedPassword);
-  const result = await Users.findOne({ where: { email, password } });
+
+  const result = await Users.findOne({ where: { email, password: criptedPassword } });
 
   return result;
 };
