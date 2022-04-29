3d4c963e724fc70c51b2ae3833334f820eb672bb
Thiago Frozzi <thiagofrozzi@hotmail.com>
2022-04-29 20:04:27 -0300

    feat: implementa rota login

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,11 +1,11 @@
 const express = require('express');
-const routes = require('../routes/routes');
+const router = require('../routes/routes');
 
 const app = express();
 
-app.use(express.json);
-app.use(routes);
+app.use(express.json());
+app.use(router);
 
-app.get('/coffee', (_req, res) => res.status(418).end());
+// app.get('/coffee', (_req, res) => res.status(418).end());
 
 module.exports = app;
--- back-end/src/services/userService.js
+++ back-end/src/services/userService.js
@@ -4,7 +4,7 @@ const Users = require('../database/models/Users');
 const login = async (email, password) => {
   const criptedPassword = md5(password);
   console.log(criptedPassword);
-  const result = Users.findOne({ where: { email, password } });
+  const result = await Users.findOne({ where: { email, password } });
 
   return result;
 };
