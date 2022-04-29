c26c0399d6875b47c59d559801b6ebf95caa44c0
Guilherme Gomes <ggs.arquiteto@gmail.com>
2022-04-29 14:16:52 -0300

    fix: resolve o problema de criação das migrations

---
+++ back-end/src/database/migrations/20220427201746-sales_products.js
@@ -0,0 +1,44 @@
+'use strict';
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.createTable('sales_products', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      sale_id: {
+        allowNull: false,
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+        references: {
+          model: 'sales',
+          key: 'id',
+        },
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      product_id: {
+        allowNull: false,
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+        references: {
+          model: 'products',
+          key: 'id',
+        },
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      quantity: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+      }
+    });
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.dropTable('sales_products');
+  }
+};
