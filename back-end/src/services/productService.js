1d7e4da309cea1ccb69c04d6c677f103d36c19b2
Guilherme Gomes <ggs.arquiteto@gmail.com>
2022-04-30 19:46:04 -0300

    feat: implementa o restante dos metodos de CRUD dos produtos

--- back-end/src/services/productService.js
+++ back-end/src/services/productService.js
@@ -1,18 +1,20 @@
 const genericService = require('./basicService');
 const { Products } = require('../database/models');
 
-const createProduct = async (data) => {
-  const result = await genericService.create(Products, data);
-  return result;
-};
+const createProduct = async (data) => genericService.create(Products, data);
 
-const editProduct = async (id, data) => {
-  const result = await genericService.update(Products, id, data);
-  return result;
-};
+const editProduct = async (id, data) => genericService.update(Products, id, data);
 
+const deleteProduct = async (id) => genericService.delete(Products, id);
+
+const findProducts = async () => genericService.read(Products);
+
+const findOneProduct = async (id) => genericService.readOne(Products, id);
 
 module.exports = {
   createProduct,
+  deleteProduct,
   editProduct,
+  findOneProduct,
+  findProducts,
 };
