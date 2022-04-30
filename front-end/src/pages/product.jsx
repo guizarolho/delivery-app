161b221a786ad955ba6597423690940f9d98fcdd
guizarolho <guimfontes7@gmail.com>
2022-04-30 01:42:03 -0300

    feat: adiciona card de produto e cria controlador de quantidade

--- front-end/src/pages/product.jsx
+++ front-end/src/pages/product.jsx
@@ -1,11 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import Navbar from '../components/Navbar';
+import ProductCard from '../components/ProductCard';
+import { requestProducts } from '../utils/requests';
 
 function Product() {
+  const navigate = useNavigate();
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetch = async () => {
+      const results = await requestProducts();
+      if (results) setProducts(results);
+    };
+    fetch();
+  }, []);
+
   return (
     <>
       <Navbar username="User" />
       <h1>Produtos</h1>
+      { products.map((e) => ProductCard(e)) }
+      <button
+        type="button"
+        data-testid="customer_products__button-cart"
+        onClick={ () => navigate('/checkout', { replace: true }) }
+      >
+        Ver Carrinho:
+        <span data-testid="customer_products__checkout-bottom-value"> R$ 0,00</span>
+      </button>
     </>
   );
 }
