23923547535e178aa9b727b0dc2d5dc2c4fe63b2
Guilherme Gomes <ggs.arquiteto@gmail.com>
2022-04-30 11:46:06 -0300

    feat: implementa a rota de registro e cria o controller de registro de novo usuário

--- back-end/src/controllers/userController.js
+++ back-end/src/controllers/userController.js
@@ -5,11 +5,17 @@ const login = async (req, res) => {
 
   const result = await userService.login(email, password);
 
-  if (!result) return res.status(404).json({ message: 'Dados invalidos' });
+  if (!result) return res.status(404).json({ message: 'Dados inválidos' });
 
   return res.status(200).json(result);
 };
 
+const register = async (req, res) => {
+  const newUser = await userService.newUser(req.body);
+  return res.status(201).json(newUser);
+};
+
 module.exports = {
   login,
+  register,
 };
--- back-end/src/routes/routes.js
+++ back-end/src/routes/routes.js
@@ -4,5 +4,6 @@ const userController = require('../controllers/userController');
 const router = Router();
 
 router.post('/login', userController.login);
+router.post('/register', userController.register);
 
 module.exports = router;
