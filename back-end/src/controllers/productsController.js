087162fa372890840110bb86c9c35639a361e03c
Guilherme Gomes <ggs.arquiteto@gmail.com>
2022-05-01 18:59:12 -0300

    feat: implementa a validação de caso o produto por id não seja encontrado

--- back-end/src/controllers/productsController.js
+++ back-end/src/controllers/productsController.js
@@ -8,6 +8,7 @@ const read = async (_req, res) => {
 const readOne = async (req, res) => {
   const { id } = req.params;
   const product = await productService.findOneProduct(id);
+  if (!product) return res.status(404).json({ message: 'Produto não encontrado' });
   return res.status(200).json(product);
 };
 
