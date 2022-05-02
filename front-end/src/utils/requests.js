8bcf2814d16dfa4ddf10bc84c162992e21315e9f
Thiago Frozzi <thiagofrozzi@hotmail.com>
2022-05-02 18:12:41 -0300

    fix: concerta erros de json.stringify no fetch

--- front-end/src/utils/requests.js
+++ front-end/src/utils/requests.js
@@ -1,21 +1,21 @@
-const JSON = 'application/json';
+const contentType = 'application/json; charset=utf-8';
 
 const requestUser = (email, password) => {
   const options = {
     method: 'POST',
-    headers: { 'Content-Type': JSON },
-    body: { email, password },
+    headers: { 'Content-Type': contentType },
+    body: JSON.stringify({ email, password }),
   };
   fetch(`http://localhost:${process.env.REACT_APP_BACKEND_PORT || '3001'}/login`, options)
     .then((response) => response.json())
     .then((data) => data);
 };
 
-const createUser = (name, email, password) => {
+const createUser = async (name, email, password) => {
   const options = {
     method: 'POST',
-    headers: { 'Content-Type': JSON },
-    body: { name, password, email },
+    headers: { 'Content-Type': contentType },
+    body: JSON.stringify({ name, password, email }),
   };
   fetch(`http://localhost:${process.env.REACT_APP_BACKEND_PORT || '3001'}/register`, options)
     .then((response) => response.json())
@@ -25,7 +25,7 @@ const createUser = (name, email, password) => {
 const requestProducts = () => {
   const options = {
     method: 'GET',
-    headers: { 'Content-Type': JSON },
+    headers: { 'Content-Type': contentType },
   };
   fetch(`http://localhost:${process.env.REACT_APP_BACKEND_PORT || '3001'}/products`, options)
     .then((response) => response.json())
