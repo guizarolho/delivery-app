e003eb814bc7a9f6673442a64d81928bcd14431a
Guilherme Gomes <ggs.arquiteto@gmail.com>
2022-05-02 09:45:31 -0300

    feat: implementa a validação de existencia do campo authorization no header

--- back-end/src/routes/routes.js
+++ back-end/src/routes/routes.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const userController = require('../controllers/userController');
 const productsController = require('../controllers/productsController');
 const jwtAuth = require('../controllers/jwtAuth');
+const validateAuthField = require('../middlewares/authField');
 
 const router = Router();
 router.use(cors());
@@ -10,6 +11,7 @@ router.use(cors());
 router.post('/login', userController.login);
 router.post('/register', userController.register);
 
+router.use(validateAuthField);
 router.use(jwtAuth);
 router.get('/products', productsController.read);
 router.get('/products/:id', productsController.readOne);
