cbdd03df34e59888178b582c7a1efb3046943ef7
guizarolho <guimfontes7@gmail.com>
2022-05-03 15:39:41 -0300

    fix: atribui valor padrão ao papel de usuário

--- back-end/src/database/models/Users.js
+++ back-end/src/database/models/Users.js
@@ -23,7 +23,7 @@ module.exports = (sequelize, DataTypes) => {
       },
       role: {
         type: DataTypes.STRING,
-        default: 'customer',
+        defaultValue: 'customer',
       }
     },
     {
