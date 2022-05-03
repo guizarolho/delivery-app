57e05f88419b1d690c03e81d18aae6a4df1b1149
Guilherme Gomes <ggs.arquiteto@gmail.com>
2022-05-03 14:04:36 -0300

    feat: implementa o controller de sales

---
+++ back-end/src/controllers/salesController.js
@@ -0,0 +1,42 @@
+const salesService = require('../services/salesService');
+
+const create = async (req, res) => {
+  try {
+    const sale = await salesService.createSale(req.body);
+    return res.status(201).json(sale);
+  } catch (e) {
+    return res.status(400).json({ message:'Não foi possível criar a venda, sentimos muito D:' });
+  }
+}
+
+const read = async (req, res) => {
+  try {
+    const sales = await salesService.readSale();
+    return res.status(200).json(sales);
+  } catch (e) {
+    return res.status(400).json({ message:'Não foi possível listar as vendas, sentimos muito D:' });
+  }
+};
+
+const readOne = async (req, res) => {
+  const sale = await salesService.readSale(req.params.id);
+  if (!sale) return res.status(404).json({ message: 'Venda não encontrada' });
+  return res.status(200).json(sale);
+};
+
+const readSaleByUserId = async (req, res) => {
+  try {
+    const sale = await salesService.readSaleByUser(req.params.id);
+    return res.status(200).json(sale);
+  } catch (e) {
+    return res.status(400).json({ message:e.message });
+  }
+};
+
+
+module.exports = {
+  create,
+  read,
+  readOne,
+  readSaleByUserId,
+};
