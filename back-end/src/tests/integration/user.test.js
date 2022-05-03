6ce89f7af54fccdc526bec1eb5532950b3758a5b
JoaoHenriqueAlmeida <joaoh.almeidaaraujo@gmail.com>
2022-05-03 17:23:31 -0300

    Merge branch 'main-group-8' of github.com:tryber/sd-014-b-project-delivery-app into main-group-8

--- back-end/src/tests/integration/user.test.js
+++ back-end/src/tests/integration/user.test.js
@@ -1,25 +1,30 @@
-import * as sinon from 'sinon';
-import * as chai from 'chai';
-import * as chaiHttp from 'chai-http';
+const sinon = require('sinon');
+const chai = require('chai');
+const  chaiHttp = require('chai-http');
 
-import { app } from '../../api/app';
-import Users from '../../database/models/Users';
-import { describe } from 'pm2';
+const app = require('../../api/app');
+const Users = require('../../database/models/Users');
+
+const { accurateLoginRequest } = require('../mocks/userMocks')
 
 chai.use(chaiHttp);
 
 const { expect } = chai;
 
-describe('', () => {
-  before(async () => {
+describe('Em caso de sucesso no método POST do endpoint /login', () => {
+  let chaiHttpRes;
 
+/*   before(async () => {
+    sinon.stub(Users, 'findOne').resolves(loginResponseMock)
   })
 
   after(async () => {
-    
-  })
+    (Users.findOne).restore()
+  }) */
 
-  it('', async () => {
-    
+  it('Deveria retornar o status code 200', async () => {
+    chaiHttpRes = await chai.request(app).post('/login').send(accurateLoginRequest)
+    const { status } = chaiHttpRes;
+    expect(status).to.equal(200);
   })
 })
---
+++ back-end/src/tests/mocks/userMocks.js
@@ -0,0 +1,8 @@
+const accurateLoginRequest = {
+    email: "fulana@deliveryapp.com",
+    password: "fulana@123",
+}
+
+module.exports = {
+  accurateLoginRequest,
+}
