aa6eb12125b83e04fbd18ca5bc694d37025665ae
JoaoHenriqueAlmeida <joaoh.almeidaaraujo@gmail.com>
2022-05-03 14:48:45 -0300

    feat: Inicia estrutura dos testes de itnegração para os endpoints de user

---
+++ back-end/src/tests/integration/user.test.js
@@ -0,0 +1,25 @@
+import * as sinon from 'sinon';
+import * as chai from 'chai';
+import * as chaiHttp from 'chai-http';
+
+import { app } from '../../api/app';
+import Users from '../../database/models/Users';
+import { describe } from 'pm2';
+
+chai.use(chaiHttp);
+
+const { expect } = chai;
+
+describe('', () => {
+  before(async () => {
+
+  })
+
+  after(async () => {
+    
+  })
+
+  it('', async () => {
+    
+  })
+})
