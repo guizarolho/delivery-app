fd5e68f699a88c260e209fdb812d935664ce67a7
JoaoHenriqueAlmeida <joaoh.almeidaaraujo@gmail.com>
2022-05-04 15:40:34 -0300

    fix: chai.use and chai.request

--- back-end/src/tests/integration/user.test.js
+++ back-end/src/tests/integration/user.test.js
@@ -6,7 +6,7 @@ const app = require('../../api/app')
 
 const { accurateLoginRequest, loginResponseMock } = require('../mocks/userMocks') ;
 
-app.use(chaiHttp);
+chai.use(chaiHttp);
 
 const { expect } = chai;
 
@@ -14,7 +14,7 @@ describe('Em caso de sucesso no método POST do endpoint /login', () => {
   let chaiHttpRes;
 
   it('Deveria retornar o status code 200', async () => {
-    chaiHttpRes = await request(app).post('/login').send(accurateLoginRequest)
+    chaiHttpRes = await chai.request(app).post('/login').send(accurateLoginRequest)
     const { status } = chaiHttpRes;
     expect(status).to.equal(200);
   })
@@ -23,7 +23,7 @@ describe('Em caso de sucesso no método POST do endpoint /login', () => {
 describe('Em caso de sucesso no método POST do endpoint /login', () => {
 
   it('Deveria retornar o nome e o cargo corretos', async () => {
-    let chaiHttpRes = await request(app).post('/login').send(accurateLoginRequest)
+    let chaiHttpRes = await chai.request(app).post('/login').send(accurateLoginRequest)
     const { name, role } = chaiHttpRes.body;
     expect(name).to.equal(loginResponseMock.name);
     expect(role).to.equal(loginResponseMock.role);
@@ -33,7 +33,7 @@ describe('Em caso de sucesso no método POST do endpoint /login', () => {
 describe('Em caso de sucesso no método POST do endpoint /login', () => {
 
   it('Deveria retornar um token válido', async () => {
-    let chaiHttpRes = await request(app).post('/login').send(accurateLoginRequest)
+    let chaiHttpRes = await chai.request(app).post('/login').send(accurateLoginRequest)
     const { token} = chaiHttpRes.body;
     expect(token).to.contain('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9');
   })
