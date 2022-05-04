1e79cc8c2f8b35e65f8251bd5742d27171863012
JoaoHenriqueAlmeida <joaoh.almeidaaraujo@gmail.com>
2022-05-04 14:54:13 -0300

    tests: testa os casos de sucesso para o endpoint /login

--- back-end/src/tests/integration/user.test.js
+++ back-end/src/tests/integration/user.test.js
@@ -1,30 +1,41 @@
-const sinon = require('sinon');
-const chai = require('chai');
-const  chaiHttp = require('chai-http');
+/* const sinon = require('sinon'); */
+import chai from 'chai';
+import chaiHttp from 'chai-http';
 
-const app = require('../../api/app');
-const Users = require('../../database/models/Users');
+import app from '../../api/app';
 
-const { accurateLoginRequest } = require('../mocks/userMocks')
 
-chai.use(chaiHttp);
+import { accurateLoginRequest, loginResponseMock } from '../mocks/userMocks';
+
+use(chaiHttp);
 
 const { expect } = chai;
 
 describe('Em caso de sucesso no método POST do endpoint /login', () => {
   let chaiHttpRes;
 
-/*   before(async () => {
-    sinon.stub(Users, 'findOne').resolves(loginResponseMock)
-  })
-
-  after(async () => {
-    (Users.findOne).restore()
-  }) */
-
   it('Deveria retornar o status code 200', async () => {
-    chaiHttpRes = await chai.request(app).post('/login').send(accurateLoginRequest)
+    chaiHttpRes = await request(app).post('/login').send(accurateLoginRequest)
     const { status } = chaiHttpRes;
     expect(status).to.equal(200);
+  })
+})
+
+describe('Em caso de sucesso no método POST do endpoint /login', () => {
+
+  it('Deveria retornar o nome e o cargo corretos', async () => {
+    let chaiHttpRes = await request(app).post('/login').send(accurateLoginRequest)
+    const { name, role } = chaiHttpRes.body;
+    expect(name).to.equal(loginResponseMock.name);
+    expect(role).to.equal(loginResponseMock.role);
+  })
+})
+
+describe('Em caso de sucesso no método POST do endpoint /login', () => {
+
+  it('Deveria retornar um token válido', async () => {
+    let chaiHttpRes = await request(app).post('/login').send(accurateLoginRequest)
+    const { token} = chaiHttpRes.body;
+    expect(token).to.contain('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9');
   })
 })
