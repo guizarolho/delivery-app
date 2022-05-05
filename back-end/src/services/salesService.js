80917492bea55ee6dbb10ad0b5c21e7461c0a576
Guilherme Gomes <ggs.arquiteto@gmail.com>
2022-05-05 18:34:42 -0300

    refactor: melhora a implementação da sale para ter o retorno esperado

--- back-end/src/services/salesService.js
+++ back-end/src/services/salesService.js
@@ -2,12 +2,29 @@ const { Op } = require('sequelize');
 const genericService = require('./basicService');
 const { Sales } = require('../database/models');
 const { SalesProducts } = require('../database/models');
+{
+
+}
 
 
 const createSale = async (data) => {
-  const sale = await genericService.create(Sales, data);
-  await SalesProducts.create({ sale_id: sale.id, product_id: data.product_id, quantity: data.quantity });
-  return sale;
+  const saleData = {
+    user_id: data.user_id,
+    seller_id: data.seller_id,
+    total_price: data.total_price,
+    delivery_address: data.delivery_address,
+    delivery_number: data.delivery_number,
+    sale_date: data.sale_date,
+    status: data.status,    
+  }
+  const sale = await genericService.create(Sales, saleData);
+  const salesProducts = Promise.all(data.vendas.map((venda) => {
+    await SalesProducts.create({ sale_id: sale.id, product_id: venda.product_id, quantity: venda.quantity });
+  }));
+  return {
+    sale,
+    salesProducts,
+  };
 };
 
 const readSales = async () => genericService.read(Sales);
