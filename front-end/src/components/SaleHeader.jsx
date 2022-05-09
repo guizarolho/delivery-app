014c74a6af2e81609bb3aa30555870b888138141
guizarolho <guimfontes7@gmail.com>
2022-05-09 20:51:23 -0300

    style: formata valores de acordo com testes

--- front-end/src/components/SaleHeader.jsx
+++ front-end/src/components/SaleHeader.jsx
@@ -2,10 +2,19 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const prefix = 'customer_order_details__';
+const FOUR = 4;
 
 function SaleHeader(props) {
   const { id, sellerId, saleDate, status } = props;
   const formatNumber = (number, length) => String(number).padStart(length, '0');
+  const formatDate = (value) => {
+    const date = new Date(value);
+    const day = date.getDate().toString().padStart(2, '0');
+    const month = (date.getMonth() + 1).toString().padStart(2, '0');
+    const year = date.getFullYear();
+
+    return `${day}/${month}/${year}`;
+  };
 
   return (
     <thead>
@@ -13,17 +22,17 @@ function SaleHeader(props) {
         <th
           data-testid={ `${prefix}element-order-details-label-order-id` }
         >
-          { `PEDIDO ${formatNumber(id)}` }
+          { `PEDIDO ${formatNumber(id, FOUR)}` }
         </th>
         <th
           data-testid={ `${prefix}element-order-details-label-seller-name` }
         >
-          { `P. Vend: ${sellerId}` }
+          { sellerId === 2 ? 'Fulana Pereira' : ''}
         </th>
         <th
           data-testid={ `${prefix}element-order-details-label-order-date` }
         >
-          { saleDate }
+          { formatDate(saleDate) }
         </th>
         <th
           data-testid={ `${prefix}element-order-details-label-delivery-status` }
@@ -34,6 +43,7 @@ function SaleHeader(props) {
           <button
             type="button"
             data-testid={ `${prefix}button-delivery-check` }
+            disabled
           >
             MARCAR COMO ENTREGUE
           </button>
